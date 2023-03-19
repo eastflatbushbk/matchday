ae619859f5874cb46fb810302a594c4f2c3b42b3
Robert Sainton <robertsainton@outlook.com>
2023-03-19 19:11:27 -0400

    add code to userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+    skip_before_action :confirm_authentication
+    def show
+        # user = User.find_by(id: session[:user_id])
+        if current_user
+          render json: @current_user, status: :ok 
+        else
+          render json: { error: "Not authorized" }, status: :unauthorized
+        end
+      end
+      def create
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: user.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def user_params
+        params.permit(:username, :age, :location, :favorite_club, :password, :password_confirmation)
+    end
+
 end
