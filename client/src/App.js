40e2fb5a036ad5799971cc5debf852e7002d202c
Robert Sainton <robertsainton@outlook.com>
2023-04-06 19:54:59 -0400

    add authcheck to  app.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,7 @@ import Login from "./components/Login";
 import { MatchProvider } from "./components/MatchContext";
  import { Routes ,Route } from "react-router-dom";
 
-//import PostAuthApp from './components/PostAuthApp';
-//import PreAuthApp from './components/PreAuthApp';
+
 import {  UserProvider } from "./components/UserContext";
 import MatchPage from "./components/MatchPage";
 import MatchForm from "./components/MatchForm";
@@ -19,56 +18,19 @@ import Errors from "./components/Errors";
 function App() {
 
 
-  // const [authCheck, setAuthCheck] = useState(false)
-  // const [currentUser, setCurrentUser] = useState(null)
-  // const {currentUser} = useContext(UserContext)
-  const [loading, setLoading] = useState(true)
- 
-  // useEffect(() => {
-  //   fetch("/me").then((response) => {
-  //     if (response.ok) {
-  //       response.json().then(user => {
-  //         console.log(user)
-  //         setCurrentUser(user)
-  //         setAuthCheck(true)})
-  //     }
-  //     else {
-  //       setAuthCheck(true)
-  //     }
-  //   })
-  // }, []);
-
-  // useEffect(() => {
-  //   fetch("/me")
-  //   .then((response) => response.json())
-  //       .then(user => {
-  //         console.log(user)
-  //         setCurrentUser(user)
-          
-  //       })
-    
-  // }, []);
-  // if(!authCheck) { console.log("not checked") }
+   const [authCheck, setAuthCheck] = useState(true)
   
-// const app = currentUser ? (
-            
-//                         <PostAuthApp  loading={ loading } currentUser = {currentUser}/>
-                     
-//                ) : (
-//          <ErrorProvider>
-//                 <UserProvider setLoading={ setLoading }>
-//                      <PreAuthApp loading={ loading }  />
-//                 </UserProvider>
-//           </ErrorProvider>
-//         )
- const routeJsx = loading ? ( <>...</> ) : (
+  // const [loading, setLoading] = useState(true)
+ 
+  
+ const routeJsx = authCheck ? ( <>...</> ) : (
 
                     <Routes>
-                          <Route exact path="/match" element={<MatchPage loading={loading}  />} />
-                          <Route exact path="/add_match" element={<MatchForm loading={loading} />} />
+                          <Route exact path="/match" element={<MatchPage authCheck={authCheck}  />} />
+                          <Route exact path="/add_match" element={<MatchForm authCheck={authCheck} />} />
                           <Route exact path="/match/:id" element={<MatchDetails      />} />
-                          <Route exact path="/edit_match" element={<EditMatchForm loading={loading}     />} />
-                          <Route exact path="/login" element={<Login loading={loading}     />} />
+                          <Route exact path="/edit_match" element={<EditMatchForm authCheck={authCheck}     />} />
+                          <Route exact path="/login" element={<Login authCheck={authCheck}     />} />
         
                             {/* <Route exact path="/profile" element={null} /> */}
         
@@ -80,7 +42,7 @@ function App() {
    
     <div>
      <ErrorProvider>
-           <UserProvider setLoading={ setLoading }>
+           <UserProvider setAuthCheck={ setAuthCheck }>
               <MatchProvider>
                  <NavBar />
                     <Errors/>
